9bfeb2dba5326c64e0b0ed952c4e8966ead6c3e0
Håvard Berland <havb@equinor.com>
2024-01-21 20:09:50 +0100

    Add support for -J

--- src/lsftoslurm/bsub.py
+++ src/lsftoslurm/bsub.py
@@ -5,18 +5,27 @@ import tempfile
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Submit jobs to Slurm via LSF syntax")
+
+    parser.add_argument("-J", "--job_name", type=str)
     parser.add_argument("shellcode", type=str)
     return parser
 
 
 def main() -> None:
     args = get_parser().parse_args()
+
+    job_name_args = []
+    if args.job_name:
+        job_name_args = ["-J", args.job_name]
+
     with tempfile.NamedTemporaryFile() as script:
         script.write("#!/bin/bash\n".encode())
         script.write(args.shellcode.encode())
         script.flush()
         result = subprocess.run(
-            ["sbatch", script.name], stdout=subprocess.PIPE, check=False
+            ["sbatch"] + job_name_args + [script.name],
+            stdout=subprocess.PIPE,
+            check=False,
         )
         if result:
             stdout = result.stdout.decode()
